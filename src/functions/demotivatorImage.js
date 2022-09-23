aebf093819b90f0ef52f3a2e01f1cf49a65d3aaa
swd-d <swdchw@gmail.com>
2022-09-23 15:51:51 +0500

    demotivatorImage update

--- src/functions/demotivatorImage.js
+++ src/functions/demotivatorImage.js
@@ -1,28 +1,54 @@
 const { createCanvas, loadImage } = require('canvas');
 
 module.exports = async(img, title, subtitle) => {
-  const canvas = createCanvas(512, 512);
-  const ctx = canvas.getContext('2d');
+  if(title.length >= 24) {
+    const canvas = createCanvas(712, 512);
+    const ctx = canvas.getContext('2d');
+    
+    const asset = await loadImage('../assets/big_demotivator.png');
+    ctx.drawImage(asset, 0, 0);
   
-  const asset = await loadImage('../assets/demotivator.png');
-  ctx.drawImage(asset, 0, 0);
-
-  const avatar = await loadImage(img);
-  ctx.drawImage(avatar, 33, 33, 446, 390);
+    const avatar = await loadImage(img);
+    ctx.drawImage(avatar, 133, 33, 446, 390);
+    
+    ctx.fillStyle = '#fff';
+    ctx.textAlign = "center";
+    ctx.textBaseline = "middle";
+    ctx.font = `28px Times New Roman`;
+    let widthText = 470
+    if(subtitle) ctx.fillText(title, 360, widthText - 10);
+    if(!subtitle) ctx.fillText(title, 360, widthText);
   
-  ctx.fillStyle = '#fff';
-  ctx.textAlign = "center";
-  ctx.textBaseline = "middle";
-  ctx.font = `29px Times New Roman`;
-
-  let widthText = 470
-  if(subtitle) ctx.fillText(title, 260, widthText - 10);
-  if(!subtitle) ctx.fillText(title, 260, widthText);
-
-  if(subtitle) {
-  ctx.font = `20px Times New Roman`;
-  ctx.fillText(subtitle, 260, widthText + 20);
+    if(subtitle) {
+    ctx.font = `20px Times New Roman`;
+    ctx.fillText(subtitle, 360, widthText + 20);
+    };
+  
+    return canvas.toBuffer();
   };
+
+    const canvas = createCanvas(512, 512);
+    const ctx = canvas.getContext('2d');
+    
+    const asset = await loadImage('../assets/demotivator.png');
+    ctx.drawImage(asset, 0, 0);
   
-  return canvas.toBuffer();
+    const avatar = await loadImage(img);
+    ctx.drawImage(avatar, 33, 33, 446, 390);
+    
+    ctx.fillStyle = '#fff';
+    ctx.textAlign = "center";
+    ctx.textBaseline = "middle";
+    ctx.font = `29px Times New Roman`;
+  
+    let widthText = 470
+    if(subtitle) ctx.fillText(title, 260, widthText - 10);
+    if(!subtitle) ctx.fillText(title, 260, widthText);
+  
+    if(subtitle) {
+    ctx.font = `20px Times New Roman`;
+    ctx.fillText(subtitle, 260, widthText + 20);
+    };
+  
+    return canvas.toBuffer();
 };
