014d1c4df98a670672a5db144fe78acad43b4ea6
swd-d <swdchw@gmail.com>
2022-09-24 18:48:54 +0500

    new function for edit img's

---
+++ src/functions/prikol.js
@@ -0,0 +1,44 @@
+const { createCanvas, loadImage, registerFont } = require('canvas');
+const random = require('../functions/random.js');
+
+registerFont('../assets/Lobster.ttf', { family: 'Lobster'});
+
+module.exports = async(text, whatYouNeed, img) => {
+    if(whatYouNeed === 'textOnImg') {
+
+    const canvas_img = await loadImage(img);
+
+    const canvas = createCanvas(canvas_img.width, canvas_img.height);
+    const ctx = canvas.getContext('2d');
+
+    ctx.drawImage(canvas_img, 0, 0);
+
+    random(0, 4) <= 2 ? ctx.fillStyle = '#fff' : ctx.fillStyle = '#000000';
+    ctx.textAlign = "center";
+    ctx.textBaseline = "middle";
+    ctx.font = '32px Lobster';
+
+
+    ctx.fillText(text, random(0, canvas_img.width), random(0,  canvas_img.height));
+
+    return canvas.toBuffer();
+    };
+    if(whatYouNeed === 'jack_fresco') {
+        const canvas_img = await loadImage(require('fs').readFileSync('../assets/jak_fresco.png'));
+
+        const canvas = createCanvas(canvas_img.width, canvas_img.height);
+        const ctx = canvas.getContext('2d');
+    
+        ctx.drawImage(canvas_img, 0, 0);
+
+        ctx.fillStyle = '#000000';
+        ctx.textAlign = "center";
+        ctx.textBaseline = "middle";
+        ctx.font = '32px Lobster';
+    
+    
+        ctx.fillText(text, 595, 200);
+    
+        return canvas.toBuffer();
+    };
+};
