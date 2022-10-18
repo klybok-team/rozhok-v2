ffeaba113fb4b553fa812a1b0d8954176c0ad875
swd-d <swdchw@gmail.com>
2022-10-18 18:37:45 +0500

    edit for new config

--- src/cmd/dem.js
+++ src/cmd/dem.js
@@ -1,4 +1,3 @@
-const { demotivatorReplies, other } = require('../../config.js')
 const downloadFile = require('../functions/downloadFile.js')
 const demotivatorImage = require('../functions/demotivatorImage.js');
 
@@ -6,9 +5,10 @@ module.exports = {
 	name: 'demotivator',
 	aliases: ['d', 'dem'],
 	async execute(client, m, args) {
+
 		args = args.toString().split(' | ');
 
-		if(!args[0]) return client.createMessage(m.channel.id, demotivatorReplies.ErrorArgs.noText);
+		if(!args[0]) return client.createMessage(m.channel.id, 'Не указан текст.');
 
 		let attachment;
 
@@ -19,19 +19,19 @@ module.exports = {
 				attachment = messageReferenceAttachments?.attachments[0];
 			};
 		};
+
 		if(m.attachments[0]) attachment = m.attachments[0];
 	
-		if(!attachment) return client.createMessage(m.channel.id, demotivatorReplies.ErrorArgs.noImg);
+		if(!attachment) return client.createMessage(m.channel.id, 'Отсутствует изображение.');
 
 		if(attachment.filename.endsWith('.jpg') || attachment.filename.endsWith('.jpeg') || attachment.filename.endsWith('.png')) {
+			if(args[0].length > 35 || args[1]?.length > 35) return client.createMessage(m.channel.id, `Упс.. много букв.\nТекст: ${args[0].length} из 35\nПод-текст ${args[1]?.length ? args[1].length : '0'} из 35`);
+			return await downloadFile(`${attachment.url}`).then(async source => {
+			let image = await demotivatorImage(source, args[0], args[1]);
 
-		if(args[0].length > 35 || args[1]?.length > 35) return client.createMessage(m.channel.id, demotivatorReplies.Errorlength.replace('{}', args[0].length).replace('{1}', args[1]?.length ? args[1].length : '0'));
-		return await downloadFile(`${attachment.url}`).then(async source => {
-
-		let image = await demotivatorImage(source, args[0], args[1]);
-		client.createMessage(m.channel.id, {}, [{ file: image, name: attachment.filename }]);
+			client.createMessage(m.channel.id, {}, [{ file: image, name: attachment.filename }]);
 		});
-		};
-		return client.createMessage(m.channel.id, other.fileExNotNed);
-	},
+	};
+		return client.createMessage(m.channel.id, 'Расширение файла не поддерживается.');
+	}
 };
--- src/cmd/lobster.js
+++ src/cmd/lobster.js
@@ -1,14 +1,13 @@
 const prikol = require('../functions/prikol.js');
-const { lobsterReplies, other } = require('../../config.js')
 const downloadFile = require('../functions/downloadFile.js')
 
 module.exports = {
 	name: 'lobster',
 	aliases: ['l'],
 	async execute(client, m, args) {
-		if(!args) return client.createMessage(m.channel.id, lobsterReplies.ErrorArgs.noText);
+		if(!args) return client.createMessage(m.channel.id, 'Не указан текст.');
 
-        if(args.lenght > 99) return client.createMessage(m.channel.id, lobsterReplies.Errorlenght);
+        if(args.lenght > 99) return client.createMessage(m.channel.id, `Слишком много текста.\n${args.lenght} из 99`);
 
 		let attachment;
 
@@ -21,7 +20,7 @@ module.exports = {
 		};
 		if(m.attachments[0]) attachment = m.attachments[0];
 	
-		if(!attachment) return client.createMessage(m.channel.id, lobsterReplies.ErrorArgs.noImg);
+		if(!attachment) return client.createMessage(m.channel.id, 'Отсутствует изображение.');
 		
 		if(attachment.filename.endsWith('.jpg') || attachment.filename.endsWith('.jpeg') || attachment.filename.endsWith('.png')) {
             return await downloadFile(`${attachment.url}`).then(async source => {
@@ -31,6 +30,6 @@ module.exports = {
             client.createMessage(m.channel.id, {}, [{ file: image, name: attachment.filename }]);
 		    });
 	    } 
-	return client.createMessage(m.channel.id, other.fileExNotNed);
+	return client.createMessage(m.channel.id, 'Расширение файла не поддерживается.');
 	}
 };
