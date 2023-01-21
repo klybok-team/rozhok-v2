b1d95684658b9622188411c769adfc5eb2663440
swd <swdchw@gmail.com>
2023-01-21 22:16:26 +0500

    add own lobster generation

--- src/cmd/lobster.js
+++ src/cmd/lobster.js
@@ -1,13 +1,17 @@
-const prikol = require('../functions/prikol.js');
 const downloadFile = require('../functions/downloadFile.js')
+const { createCanvas, loadImage, registerFont } = require('canvas');
+const random = require('../functions/random.js')
+
+registerFont('../assets/Lobster.ttf', { family: 'Lobster' });
 
 module.exports = {
 	name: 'lobster',
 	aliases: ['l'],
+	desc: 'Создать смешную надпись на картинке',
 	async execute(client, m, args) {
 		if(!args) return client.createMessage(m.channel.id, 'Не указан текст.');
 
-        if(args.lenght > 99) return client.createMessage(m.channel.id, `Слишком много текста.\n${args.lenght} из 99`);
+        if(args.length > 99) return client.createMessage(m.channel.id, `Слишком много текста.\n${args.lenght} из 99`);
 
 		let attachment;
 
@@ -25,9 +29,22 @@ module.exports = {
 		if(attachment.filename.endsWith('.jpg') || attachment.filename.endsWith('.jpeg') || attachment.filename.endsWith('.png')) {
             return await downloadFile(`${attachment.url}`).then(async source => {
 
-            let image = await prikol(args, 'textOnImg', source);
+			const canvas_img = await loadImage(source);
+			const canvas = createCanvas(canvas_img.width, canvas_img.height);
+			const ctx = canvas.getContext('2d');
+	
+			ctx.drawImage(canvas_img, 0, 0);
+			
+			ctx.fillStyle = '#fff'
+			ctx.textAlign = "center";
+			ctx.textBaseline = "middle";
+			ctx.font = '32px Lobster';
+			
+			ctx.fillText(args, random(0, canvas_img.width), random(0, canvas_img.height));
 
-            client.createMessage(m.channel.id, {}, [{ file: image, name: attachment.filename }]);
+			const result = canvas.toBuffer();
+
+            client.createMessage(m.channel.id, {}, [{ file: result, name: attachment.filename }]);
 		    });
 	    } 
 	return client.createMessage(m.channel.id, 'Расширение файла не поддерживается.');
